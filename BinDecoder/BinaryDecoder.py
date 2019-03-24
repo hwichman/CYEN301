4cda2301fa0574c31e9f0cc07712a977e828f9a4
LAPTOP-NJ1O6TR1\Rain <haleyrainn@gmail.com>
2019-03-24 14:47:07 -0500

    Created the binary decoder
    Not optimized, does not autodetect 7 bit vs 8 bit binary strings

---
+++ BinDecoder/BinaryDecoder.py
@@ -0,0 +1,34 @@
+#Binary Decoder
+import sys
+def decode(grouping,binaryString):
+    #detect if it is an 8 bit or 7 bit
+    #split the string into groups of "grouping", put data in an array
+    #initialize a string array
+    groupings = []
+    i = 0
+    currentBinaryString = ""
+    for char in binaryString:
+        if (i != 0 and i % grouping == 0):
+            groupings.append(currentBinaryString)
+            currentBinaryString = char
+        elif (i == len(binaryString)-1):
+            currentBinaryString += char
+            groupings.append(currentBinaryString)
+        else:
+            currentBinaryString += char
+        i += 1
+    #convert each grouping into a character
+    decodedMessage = ""
+    for binaryGrouping in groupings:
+        n = int(binaryGrouping,2)
+        if (n == 8):
+            decodedMessage = decodedMessage[:len(decodedMessage)-1]
+        if (n!=8 and n>0 and n<128):
+            decodedMessage += chr(n)
+    #return the resulting string
+    return decodedMessage
+
+#main
+for line in stdin:
+    sys.stdout.write(decode(7,line))
+    sys.stdout.write(decode(8,line))
