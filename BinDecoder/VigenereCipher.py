7e413380d726282688dcdf158a38e2af182eaf5c
stella81088 <35810953+stella81088@users.noreply.github.com>
2019-03-24 19:22:54 -0500

    Update VigenereCipher.py

--- BinDecoder/VigenereCipher.py
+++ BinDecoder/VigenereCipher.py
@@ -1,1 +1,16 @@
-
+##########################################################################
+#
+#   Team: Incas
+#
+#   Members:    Matt LaCroix
+#               Ben Doughty
+#               Stella Li
+#               Seth Martin
+#               Haley Wichman
+#               Richard
+#
+#   Assignment 2 - Vingenere Cipher
+#
+#   Git Repo: https://github.com/hwichman/CYEN301/
+#
+##########################################################################
