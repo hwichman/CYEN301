d774601a3cfe72f29ff6e352e6a22ca55a282967
LAPTOP-NJ1O6TR1\Rain <haleyrainn@gmail.com>
2019-04-02 08:31:40 -0500

    Create fetch.py

---
+++ FTPCovertChannel/fetch.py
@@ -0,0 +1,22 @@
+##########################################################################
+#
+#   Team: Incas
+#
+#   Members:    Matt LaCroix
+#               Ben Doughty, btd001@latech.edu
+#               Stella Li
+#               Seth Martin
+#               Haley Wichman
+#               Richard Rose
+#               Bibhut Khadka
+#
+#   Assignment 1 - Binary Decoder
+#
+#   Git Repo: https://github.com/hwichman/CYEN301/
+#
+##########################################################################
+
+#FTP Covert Channel Decoder
+
+
+
