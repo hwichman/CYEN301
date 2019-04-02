97d523eb2546dddaf3851b5004595bfc39fbd47f
btdoughty <platypirfun@gmail.com>
2019-04-02 18:17:11 -0500

    Testing FTP commands in pythong: connect to FTP server, print file list

--- FTPCovertChannel/fetch.py
+++ FTPCovertChannel/fetch.py
@@ -10,13 +10,24 @@
 #               Richard Rose
 #               Bibhut Khadka
 #
-#   Assignment 1 - Binary Decoder
-#
+#   Assignment 2 - FTP (storage) Covert Channel
 #   Git Repo: https://github.com/hwichman/CYEN301/
 #
 ##########################################################################
 
 #FTP Covert Channel Decoder
+
+from ftplib import FTP
+
+ftp = ftplib.FTP('jeangourd.com','anonymous','')
+
+print "File List: "
+
+files = ftp.dir()
+
+print files
+
+
 
 
 
