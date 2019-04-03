5f3fed3f7ce72bc65de7cff6163db2f2db6fc0a7
LAPTOP-NJ1O6TR1\Rain <haleyrainn@gmail.com>
2019-04-02 20:26:13 -0500

    Update fetch.py
    Should print mmC0v3rgt or similar as of now, which doesn't seem right so I have a couple questions for gourd. Must be connected to school wifi to work. Also, left a couple print statements for debugging, and didn't get a chance to test the METHOD function since I made it off campus.

--- FTPCovertChannel/fetch.py
+++ FTPCovertChannel/fetch.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 ##########################################################################
 #
 #   Team: Incas
@@ -6,7 +7,7 @@
 #               Ben Doughty, btd001@latech.edu
 #               Stella Li
 #               Seth Martin
-#               Haley Wichman
+#               Haley Wichman, hrw010@latech.edu
 #               Richard Rose
 #               Bibhut Khadka
 #
@@ -18,16 +19,68 @@
 #FTP Covert Channel Decoder
 
 from ftplib import FTP
+def METHOD(files):
+    for line in files:
+        if (line[:3] != "---"):
+            return 10
+    return 7
 
-ftp = ftplib.FTP('jeangourd.com','anonymous','')
+def decode(grouping,binaryString):
+    #detect if it is an 8 bit or 7 bit
+    #split the string into groups of "grouping", put data in an array
+    #initialize a string array
+    groupings = []
+    i = 0
+    currentBinaryString = ""
+    for char in binaryString:
+        if (i != 0 and i % grouping == 0):
+            groupings.append(currentBinaryString)
+            currentBinaryString = char
+        elif (i == len(binaryString)-1):
+            currentBinaryString += char
+            groupings.append(currentBinaryString)
+        else:
+            currentBinaryString += char
+        i += 1
+    #convert each grouping into a character
+    decodedMessage = ""
+    for binaryGrouping in groupings:
+        n = int(binaryGrouping,2)
+        if (n == 8):
+            decodedMessage = decodedMessage[:len(decodedMessage)-1]
+        if (n!=8 and n>31 and n<128):
+            decodedMessage += chr(n)
+    #return the resulting string
+    return decodedMessage
 
-print "File List: "
 
-files = ftp.dir()
+#The number of bits it reads in each line
+MAX_READTYPE = 10
+ftp = FTP('jeangourd.com','anonymous','')
 
-print files
+files = []
+ftp.dir(files.append)
 
+#readType = METHOD(files)
+readType = 7
 
-
-
-
+charString = ""
+if (readType <= MAX_READTYPE and readType > 0):
+    for line in files:
+        print (line)
+        charString += line[10-readType:10]
+else:
+    print ("Invalid read type range: should be 0 to ", MAX_READTYPE)
+print (charString)
+binaryString = ""
+for char in charString:
+	if (char == '-'):
+		binaryString += '0';
+	else:
+		binaryString += '1';
+print (binaryString)
+	
+print (decode(7, binaryString))
+print ("\n")
+print (decode(8, binaryString))
+print ("\n")
