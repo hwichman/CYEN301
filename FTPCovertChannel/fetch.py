c445b8e6ccbd28df07884ff6305b69a65cf2aeaa
LAPTOP-NJ1O6TR1\Rain <haleyrainn@gmail.com>
2019-04-04 20:53:25 -0500

    removed fetch.py, renamed fetch2.py to fetch.py

--- FTPCovertChannel/fetch.py
+++ FTPCovertChannel/fetch.py
@@ -18,9 +18,17 @@
 
 #FTP Covert Channel Decoder
 
-from ftplib import FTP
-METHOD = 10
+import ftplib
 
+#allows you to use methods "7" or "10" to decode the FTP covert channel
+#method "7" - skips files/folders where the first 3 permission bits are not "---"
+# this method also decodes using 7-bit groupings only
+#method "10" - uses all 10 permission bits; this uses all files/folders and concatenates all files in a directory
+
+METHOD = 10      #can be 7 or 10 based on method used
+workdir = "10"   #allows to change the working directory, can be "main", "7" or "10" based on directory
+
+#decoder
 def decode(grouping,binaryString):
     #detect if it is an 8 bit or 7 bit
     #split the string into groups of "grouping", put data in an array
@@ -49,35 +57,41 @@ def decode(grouping,binaryString):
     #return the resulting string
     return decodedMessage
 
+#connects to ftp server
+ftp = ftplib.FTP('jeangourd.com','anonymous','')
 
-#The number of bits it reads in each line
-MAX_READTYPE = 10
-ftp = FTP('jeangourd.com','anonymous','')
+#changes working directory if necessary
+if (workdir != 'main'):
+    ftp.cwd(workdir)
 
-if (METHOD == 7):
-    ftp.cwd("7")
-elif (METHOD == 10):
-    ftp.cwd("10")
+#empty list to contain strings of file information
 files = []
+
+#adds file information of each file to array
 ftp.dir(files.append)
 
-readType = METHOD
+#truncates file information to first 10 characters each
+for index in range(len(files)):
+    files[index] = files[index][:10]
 
-charString = ""
-if (readType <= MAX_READTYPE and readType > 0):
-    for line in files:
-        # only adds string if first 3 chars are "---"
-        if (line[:3] == "---" or readType == 10):
-            charString += line[10-readType:10]
-        
-else:
-    print ("Invalid read type range: should be 0 to ", MAX_READTYPE)
-binaryString = ""
-for char in charString:
-	if (char == '-'):
-		binaryString += '0';
-	else:
-		binaryString += '1';
-	
-print (decode(7, binaryString))
-print ("\n")
+#"files" now contains permission strings for every file in the directory
+#builds a single string which will be fed into the decoder
+covertmessage = ""
+for permission in files:
+    if (METHOD == 10) or (permission[:3] == "---"):
+        for letter in permission[10-METHOD:10]:
+            if (letter == "-"):
+                covertmessage += '0'
+            else:
+                covertmessage += '1'
+
+#main
+print(decode(7, covertmessage))
+#if method "10" is used, this well also output an 8-bit-grouping variant of the decoded message
+#if (METHOD == 10):
+#    print(decode(8, covertmessage))
+
+
+
+
+
