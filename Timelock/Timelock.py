0ab84a5bd55f12d3a37e79a5b8be164a644f0e2e
LAPTOP-NJ1O6TR1\Rain <haleyrainn@gmail.com>
2019-04-15 19:44:05 -0500

    Finished for the most part(Input is still hard-coded)
    Needs to get user input and the current system time instead of it being hard coded

--- Timelock/Timelock.py
+++ Timelock/Timelock.py
@@ -18,11 +18,13 @@
 
 #Timelock
 import datetime
-import hashlib
-currentTime = "2013 05 06 07 43 25"
+import md5
+currentTime = "2017 04 26 15 14 30"
 #currentTime = datetime.now(tzinfo=None)
-epochTime= "1999 12 31 23 59 59"
+epochTime= "1974 06 01 08 57 23"
 
+####Daylight savings stuff####
+#a class containing the start month/day and end month/day of daylight savings
 class dst():
 	def __init__(self, startMonth, startDay, endMonth, endDay):
 		self.startMonth = startMonth
@@ -30,8 +32,20 @@ class dst():
 		self.endMonth = endMonth
 		self.endDay = endDay
 
-dstDict = {1966:dst(4,24,10,30), 1999:dst(4,4,10,31), 2013:dst(3,10,11,3)}
-	
+#A historical list of daylight savings date and times
+dstDict = {1966:dst(4,24,10,30),1967:dst(4,30,10,29),1968:dst(4,28,10,27),1969:dst(4,27,10,26),1970:dst(4,26,10,25), \
+           1971:dst(4,25,10,31),1972:dst(4,30,10,29),1973:dst(4,29,10,28),1974:dst(1, 6,10,27),1975:dst(2,23,10,26), \
+           1976:dst(4,25,10,31),1977:dst(4,24,10,30),1978:dst(4,30,10,29),1979:dst(4,29,10,28),1980:dst(4,27,10,26), \
+           1981:dst(4,26,10,25),1982:dst(4,25,10,31),1983:dst(4,24,10,30),1984:dst(4,29,10,28),1985:dst(4,28,10,27), \
+           1986:dst(4,27,10,26),1987:dst(4, 5,10,25),1988:dst(4, 3,10,30),1989:dst(4, 2,10,29),1990:dst(4, 1,10,28), \
+           1991:dst(4, 7,10,27),1992:dst(4, 5,10,25),1993:dst(4, 4,10,31),1994:dst(4, 3,10,30),1995:dst(4, 2,10,29), \
+           1996:dst(4, 7,10,27),1997:dst(4, 6,10,26),1998:dst(4, 5,10,25),1999:dst(4, 4,10,31),2000:dst(4, 2,10,29), \
+           2001:dst(4, 1,10,28),2002:dst(4, 7,10,27),2003:dst(4, 6,10,26),2004:dst(4, 4,10,31),2005:dst(4, 3,10,30), \
+           2006:dst(4, 2,10,29),2007:dst(3,11,11, 4),2008:dst(3, 9,11, 2),2009:dst(3, 8,11, 1),2010:dst(3,14,11, 7), \
+           2011:dst(3,13,11, 6),2012:dst(3,11,11, 4),2013:dst(3,10,11, 3),2014:dst(3, 9,11, 2),2015:dst(3, 8,11, 1), \
+           2016:dst(3,13,11, 6),2017:dst(3,12,11, 5),2018:dst(3,11,11, 4),2019:dst(3,10,11, 3),2020:dst(3, 8,11, 1)}
+
+#returns true if one datetime is in daylight savings while the other is not	
 def DSTApplies(dateTime1, dateTime2):
 	if (isInDST(dateTime1) and isInDST(dateTime2)):
 		return False
@@ -39,7 +53,8 @@ def DSTApplies(dateTime1, dateTime2):
 		return False
 	else:
 		return True
-	
+
+#returns true if the datetime object is in daylight savings time	
 def isInDST(dateTimeObj):
 	year = dateTimeObj.year
 	dstYear = dstDict[year]
@@ -50,10 +65,14 @@ def isInDST(dateTimeObj):
 	else:
 		return False
 
+####Conversion stuff####
 def timeStringToDatetime(timeString):
 	return datetime.datetime(int(timeString[:4]),int(timeString[5:7]),int(timeString[8:10]), int(timeString[11:13]), int(timeString[14:16]), int(timeString[17:19]))
 
+#takes the MD5 digest as a parameter and returns a code that is 4 characters long: the first two characters are the first two alphabetic characters
+#starting from the left of the digest and the first two numeric characters starting from the left of the digest
 def digestedTimeDifferenceToCode(digestedTime):
+        #string variables to append characters to
 	codeLetters = ""
 	codeNumbers = ""
 	for i in range(0,len(digestedTime)):
@@ -68,35 +87,30 @@ def digestedTimeDifferenceToCode(digestedTime):
 	code = codeLetters+codeNumbers
 	return code
 
-def addtzOffset(dateTimeObj, offsetHrs):
-	dateTimeObj.hour = dateTimeObj.hour + offsetHrs
-
-#Main
-epochTime = timeStringToDatetime(epochTime)
-currentTime = timeStringToDatetime(currentTime)
+######   MAIN    #######
+dtEpochTime = timeStringToDatetime(epochTime)
+dtCurrentTime = timeStringToDatetime(currentTime)
 
 
 #calculate the time difference
-timeDifference = currentTime - epochTime
+timeDifference = dtCurrentTime - dtEpochTime
 
-print ("time difference: ",timeDifference)
-
-#convert the timedelta to an integer time difference in seconds and apply daylight savings if needed
+#convert the timedelta to an integer time difference in seconds
 timeDifferenceInSeconds = 86400*timeDifference.days + timeDifference.seconds
-if (DSTApplies(epochTime, currentTime)):
+
+#applies daylight savings (if one time xor the other time is in dst)
+if (DSTApplies(dtEpochTime, dtCurrentTime)):
 	timeDifferenceInSeconds -= 3600
-print ("time difference in seconds: ",timeDifferenceInSeconds)
 
 #rounds the time down to the nearest divisibility by 60
 usableTimeDifference = timeDifferenceInSeconds - timeDifferenceInSeconds%60
-print ("usable time difference: ",usableTimeDifference)
 
 #digest this usableTime difference twice
-digestedTime = hashlib.md5(hashlib.md5(str(usableTimeDifference)).hexdigest()).hexdigest()
-print ("digestedTime: ",digestedTime)
+digestedTime = md5.md5(md5.md5(str(usableTimeDifference)).hexdigest()).hexdigest()
 
-#takes the first two alphabetic characters from the right and the first two numeric from the left to get the code
+#takes the first two alphabetic characters starting from the left and the first two numeric starting from the right to get the code
 code = digestedTimeDifferenceToCode(digestedTime)
-
+print ("Current Time: "+currentTime)
+print ("Epoch Time  : "+epochTime)
 print (code)
 
