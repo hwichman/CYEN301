223389a6c0c9b365c56d9c716b93a772cbe52b6a
stella81088 <35810953+stella81088@users.noreply.github.com>
2019-04-21 21:39:33 -0500

    Update ChatCovertChannel.py
    can anyone make use of this?

--- ChatCovertChannel/ChatCovertChannel.py
+++ ChatCovertChannel/ChatCovertChannel.py
@@ -17,3 +17,102 @@
 ##########################################################################
 
 #Chat(timing)
+
+import socket
+import time
+import sys
+from binascii import hexlify
+from time import time
+
+
+#using constants
+ZERO = 0.025
+ONE = 0.1
+
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)       #create a socket
+    print = "socket sucessfully created"
+
+except socket.error as err:
+    print "socket creation failed with with error %s" %(err)
+
+s.connect((ip,port))    #connect serve on some ip and port
+port = 1337
+s.bind(("", port))      #bind the socket to port 1337
+s.listen(0)     #listen to connections
+
+c,addr = s.accept()
+
+msg = "Some message..."     #send message one character at a time
+n = 0
+for i in msg:
+    c.send(i)   #delay in between characters
+    if (covert_bin[n] == "0"):
+        time.sleep(ZERO)
+    else:
+        time.sleep(ONE)
+    n = (n + 1) % len(covert_bin)
+
+c.send("EOF")
+c.close()
+
+#convert transmittion to binary
+covert = "secret" + "EOF"
+covert_bin = ""
+
+#receive data until the string "EOF"
+t0 = time()
+data = s.recv(4096)
+t1 = time()
+delta = round(t1 - t0, 3)
+if (delta >= ONE):
+    covert_bin += "1"
+else:
+    covert_bin += "0"
+
+while (data.rstrip("\n") != "EOF"):
+    sys.stdout.write(data)
+    sys.stdout.flush()
+s.close()
+
+for i in covert:
+    # convert each character to a full byte
+    # hexlify converts ASCII to hex
+    # int converts the hex to a decimal integer
+    # bin provides its binary representation (with a 0b
+    # prefix that must be removed)
+    # that's the [2:] (return the string from the third
+    # character on)
+    # zfill left-pads the bit string with 0s to ensure a
+    # full byte
+    
+    covert_bin += bin(int(hexlify(i), 16))[2:].zfill(8)
+
+covert = ""
+i = 0
+while (i < len(covert_bin)):
+    #process one byte at a time
+    b = covert_bin[i:i + 8]
+    #covert it to ASCII
+    n = int("0b{}".format(b), 2)
+    try:
+        covert += unhexlify("{0:x}".format(n))
+    except TypeErorr:
+        covert += "?"
+    # stop at the string "EOF"
+    i += 8
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
