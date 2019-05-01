3c5096b062a7ba80efda7f6e8092b7ee4053d782
LAPTOP-NJ1O6TR1\Rain <haleyrainn@gmail.com>
2019-04-30 22:02:27 -0500

    parsing the command line and getting the arguments
    could have better error checking though. works if inputs are valid

--- Steg/steg.py
+++ Steg/steg.py
@@ -17,3 +17,73 @@
 ##########################################################################
 
 #Steg
+import sys
+
+def parse(argv):
+    #Defaults
+    method = "b"
+    direction = "s"
+    offset = 0
+    interval = 0
+    wrapperfile = "wrapper.txt"
+    hiddenfile = "hidden.txt"
+
+    #invalid argument flag
+    invalid = 0
+    
+    if (argv[0] == "-b"):
+        method = "b"
+    elif (argv[0] == "-B"):
+        method = "B"
+    else:
+        invalid = 1
+    if (argv[1] == "-s"):
+        direction = "s"
+    elif (argv[1] == "-r"):
+        direction = "r"
+    else:
+        invalid = 1
+    if (argv[2][0:2] == "-o"):
+        if (argv[2][2:] != None):
+            offset = argv[2][2:]
+        else:
+            invalid = 1
+    indnum = 3
+    if (argv[indnum][0:2] == "-i"):
+        interval = argv[indnum][2:]
+        indnum+=1
+    else:
+        invalid = 1
+    if (argv[indnum][0:2] == "-w"):
+        wrapperfile = argv[indnum][2:]
+        indnum+=1
+    else:
+        invalid = 1
+    if (len(argv)!= indnum):
+        if (argv[indnum][0:2] == "-h"):
+            hiddenfile = argv[indnum][2:]
+        else:
+            invalid = 1
+    return method, direction, offset, interval, wrapperfile, hiddenfile, invalid      
+
+#MAIN
+variables = parse(sys.argv[1:])
+if (variables[6] == 1):
+    sys.stdout.write("Invalid argument")
+else:
+    method = variables[0]
+    direction = variables[1]
+    offset = variables[2]
+    interval = variables[3]
+    wrapperfile = variables[4]
+    hiddenfile = variables[5]
+
+    #debug print statements
+    print ("method:",method)
+    print ("direction:",direction)
+    print ("offset:",offset)
+    print ("interval:",interval)
+    print ("wrapperfile:",wrapperfile)
+    print ("hiddenfile:",hiddenfile)
+
+
